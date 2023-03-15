c0cc8c62a5ff38d05fdf31453b675b60c1e6dd81
Catapumz <carloscataronda@gmail.com>
2023-03-15 19:16:54 +0100

    para subir

--- src/app.js
+++ src/app.js
@@ -2,20 +2,9 @@ const express = require("express"); //asignamos a express las funciones de la li
 const cors = require("cors"); // lo mismo que arriba pero conn cors
 
 const routes_listar = require("./routes/listar_general");
-const routes_listar_25 = require("./routes/listar_25");
-const routes_listar_15 = require("./routes/listar_15");
 
-const routes_listar_bloque_15_dificultad = require("./routes/listar_bloque_15_dificultad");
-const routes_listar_travesia_15_dificultad = require("./routes/listar_travesia_15_dificultad");
-const routes_listar_bloque_25_dificultad = require("./routes/listar_bloque_25_dificultad");
-const routes_listar_travesia_25_dificultad = require("./routes/listar_travesia_25_dificultad");
-
-const routes_buscar_25 = require("./routes/buscar_25");
-const routes_buscar_15 = require("./routes/buscar_15");
-
-const routes_listar_travesia_25 = require("./routes/listar_travesia_25");
-
-const routes_listar_bloque_25 = require("./routes/listar_bloque_25");
+// const routes_buscar_25 = require("./routes/buscar_25");
+// const routes_buscar_15 = require("./routes/buscar_15");
 
 const routes_guardar = require("./routes/guardar_general");
 const routes_borrar = require("./routes/borrar_general");
@@ -34,38 +23,13 @@ app.use(express.urlencoded({ extended: true })); //recibir datos form-urlencoded
 //Crear rutas
 //listar general
 app.use("/", routes_listar);
-//listar 15
 
-app.use("/", routes_listar_15);
-//listar 25
-
-app.use("/", routes_listar_25);
-//listar bloques
-
-app.use("/", routes_listar_bloque_25);
-//listar travesias
-
-app.use("/", routes_listar_travesia_25);
-//listar bloques por dificultad
-
-app.use("/", routes_listar_bloque_15_dificultad);
-app.use("/", routes_listar_bloque_25_dificultad);
-//listar travesias por dificultad
-
-app.use("/", routes_listar_travesia_15_dificultad);
-app.use("/", routes_listar_travesia_25_dificultad);
-//buscar en 15
-
-app.use("/", routes_buscar_15);
-//buscar en 25
-
-app.use("/", routes_buscar_25);
 //guardar una entrada
-
 app.use("/", routes_guardar);
-//borrar una entrada
 
+//borrar una entrada
 app.use("/", routes_borrar);
+
 //editar una entrada
 
 app.use("/", routes_editar);
